02242e578ad855d1f9aa8c29f1579ee91b73ba57
changjo <wowchangjo@gmail.com>
2021-09-25 20:24:38 +0900

    Upload pdfs in "a href" tags.

--- script.py
+++ script.py
@@ -1,29 +1,88 @@
-token_v2 = "your_token_id"
-page_url = "notion_url"
-root_dir = "root_dir"
-from notion.client import NotionClient
-from md2notion.upload import upload
-from notion.block import PageBlock
+import os
 import io
-from pathlib import Path
 import glob
+import json
+import argparse
+from urllib import parse
+from unicodedata import normalize
+from pathlib import Path
+
 from tqdm import tqdm
+from bs4 import BeautifulSoup
+from notion.client import NotionClient
+from md2notion.upload import upload, uploadBlock
+from notion.block import PageBlock, PDFBlock, EmbedOrUploadBlock
 
-client = NotionClient(token_v2=token_v2)
-page = client.get_block(notion_url)
 
-for fname in tqdm(glob.glob("{root_dir}/**/*.md", recursive=True)):
-    with open(fname, "r", encoding="utf-8") as mdFile:
+def get_a_href_values(md_txt):
+    values = []
+    soup = BeautifulSoup(md_txt, "html.parser")
+    for link in soup.findAll('a'):
+        value = link.get('href')
+        values.append(value)
+    return values
 
-        md_txt = mdFile.read().split("\n")
-        page_title = md_txt[0].replace("#", "").strip()
-        md_txt = "\n".join(md_txt[1:-1])
 
-        mdFile = io.StringIO(md_txt)
-        mdFile.__dict__["name"] = fname #Set this so we can resolve images later
-        newPage = page.children.add_new(PageBlock, title=page_title)
+def remove_a_href(md_txt):
+    soup = BeautifulSoup(md_txt, "html.parser")
+    while soup.a is not None:
+        soup.a.replace_with('')
+    return soup.text
 
-        def convertImagePath(imagePath, mdFilePath):
-            ret = Path(mdFilePath).parent / Path(mdFilePath).stem / Path(imagePath).name
-            return ret
-        upload(mdFile, newPage, imagePathFunc=convertImagePath)
+
+def run(prop, page, file_list):
+    for fname in tqdm(file_list):
+        with open(fname, "r", encoding="utf-8") as mdFile:
+
+            md_txt = mdFile.read().split("\n")
+            page_title = md_txt[0].replace("#", "").strip()
+            md_txt = "\n".join(md_txt[1:])
+            # print('md_txt:', md_txt)
+
+            a_href_values = get_a_href_values(md_txt)
+            md_txt = remove_a_href(md_txt)
+
+            mdFile = io.StringIO(md_txt)
+            mdFile.__dict__["name"] = fname #Set this so we can resolve images later
+            newPage = page.children.add_new(PageBlock, title=page_title)
+
+            dirname = os.path.join(prop['root_dir'], os.path.splitext(fname)[0])
+            block_desc_list = []
+            for value in a_href_values:
+                block_desc = {'type': EmbedOrUploadBlock, 'source': os.path.join(dirname, value)}
+                block_desc_list.append(block_desc)
+                # print(block_desc)
+
+            def convertImagePath(imagePath, mdFilePath):
+                mdFilePath_dirname = os.path.dirname(mdFilePath)
+                imagePath = parse.unquote(imagePath)
+                abs_image_path = os.path.join(mdFilePath_dirname, imagePath)
+                ret = Path(abs_image_path)
+                return ret
+
+            upload(mdFile, newPage, imagePathFunc=convertImagePath)
+
+            for block_desc in block_desc_list:
+                uploadBlock(block_desc, newPage, mdFile.name, imagePathFunc=None)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Bear to Notion Uploader')
+    parser.add_argument('-j', '--json', type=str, default='property.json', help="Property json file")
+    args = parser.parse_args()
+    with open(args.json, 'r') as f:
+        prop = json.load(f)
+
+    client = NotionClient(token_v2=prop['token_v2'])
+    page = client.get_block(prop['page_url'])
+    file_list = sorted(glob.glob("{}/**/*.md".format(prop['root_dir']), recursive=True))
+
+    nfd_file_list = file_list
+    nfc_file_list = []
+    for nfd_file in nfd_file_list:
+        nfc_file = normalize("NFC", nfd_file)
+        nfc_file_list.append(nfc_file)
+
+    file_list = nfc_file_list
+
+    run(prop, page, file_list)
